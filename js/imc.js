a854cce7f10affb4afe4029faf68fea257aadc3c
Fsouza-sys <francisco.f.souza24@gmail.com>
2026-05-06 10:53:08 -0300

    calculadora IMC

---
+++ js/imc.js
@@ -0,0 +1,29 @@
+// criando calculo imc
+let peso = 110
+let alturaM = 1.85
+let resp = peso / (alturaM * alturaM)
+let situacao
+
+if(resp<=18.5){
+    situacao = "Abaixo do peso normal"
+}else if(resp<24.9){
+    situacao = "Peso normal"
+}else if(resp<29.9){
+    situacao = "Excesso de peso"
+}else if(resp<34.9){
+    situacao = "Obesidade classe I"
+}else if(resp<39.9){
+    situacao = "Obesidade classe II"
+}else if(resp>=40.0){
+    situacao = "Obesidade classe III"
+}
+
+let imc = document.getElementById("imc")
+let classificacao = document.getElementById("classificacao")
+let alturaText = document.getElementById("alturaText")
+let pesoText = document.getElementById("pesoText")
+
+classificacao.textContent = situacao
+imc.textContent = resp
+alturaText.textContent = alturaM
+pesoText.textContent = peso
